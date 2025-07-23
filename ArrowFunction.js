9e9ab3285ac08fb8db44f2337d7e2c3ad2638070
Goutam Tiwari <102357588+Goutam990@users.noreply.github.com>
2025-07-23 17:13:05 +0530

    Add files via upload
    Arrow function

---
+++ ArrowFunction.js
@@ -0,0 +1,16 @@
+// Regular Function
+function addtwo( num1,  num2){
+    return num1 + num2;
+}
+
+console.log(addtwo(3,4));
+
+
+// Arrow Function
+const addtwobyarrow = (num1,num2) => (num1 + num2);
+console.log(addtwo(4,5));
+
+console.log(globalThis); // nodejs---> (this) points to the global obj
+
+console.log(this);       // {} - empty object return --> node.js --> this refers to the global object
+
